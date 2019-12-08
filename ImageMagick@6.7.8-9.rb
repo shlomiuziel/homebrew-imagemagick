47a566d1ffb27b832a5438284720b15461624441
shlomiuziel <shlomi.uziel@gmail.com>
2019-12-08 11:21:29 +0200

    ImageMagick 6.7.8-9 Initial commit Also added sources for 6.7.8-9 and 6.7.8-10 (Both tars tested to build properly)

---
+++ ImageMagick@6.7.8-9.rb
@@ -0,0 +1,65 @@
+class ImagemagickAT6789 < Formula
+  desc "Tools and libraries to manipulate images in many formats"
+  homepage "https://www.imagemagick.org/"
+  # Please always keep the Homebrew mirror as the primary URL as the
+  # ImageMagick site removes tarballs regularly which means we get issues
+  # unnecessarily and older versions of the formula are broken.
+  url "https://launchpad.net/imagemagick/main/6.7.8-9/+download/ImageMagick-6.7.8-9.tar.gz"
+  mirror "https://raw.githubusercontent.com/shlomiuziel/homebrew-imagemagick/ImageMagick-6.7.8-9.tar.gz"
+  sha256 "c366b18830e8ddca0352b6cb18bbe77d971bb739a6410129c76e5552358ebf93"
+  head "https://github.com/imagemagick/imagemagick6.git"
+
+
+  keg_only :versioned_formula
+
+  depends_on "pkg-config" => :build
+
+  depends_on "freetype"
+  depends_on "jpeg"
+  depends_on "libpng"
+  depends_on "libtiff"
+  depends_on "libtool"
+  depends_on "little-cms2"
+  depends_on "openjpeg"
+  depends_on "webp"
+  depends_on "xz"
+
+  skip_clean :la
+
+  def install
+    args = %W[
+      --disable-osx-universal-binary
+      --prefix=#{prefix}
+      --disable-dependency-tracking
+      --disable-silent-rules
+      --disable-opencl
+      --disable-openmp
+      --enable-shared
+      --enable-static
+      --with-freetype=yes
+      --with-modules
+      --with-webp=yes
+      --with-openjp2
+      --without-gslib
+      --with-gs-font-dir=#{HOMEBREW_PREFIX}/share/ghostscript/fonts
+      --without-fftw
+      --without-pango
+      --without-x
+      --without-wmf
+    ]
+
+    # versioned stuff in main tree is pointless for us
+    inreplace "configure", "${PACKAGE_NAME}-${PACKAGE_VERSION}", "${PACKAGE_NAME}"
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  test do
+    assert_match "PNG", shell_output("#{bin}/identify #{test_fixtures("test.png")}")
+    # Check support for recommended features and delegates.
+    features = shell_output("#{bin}/convert -version")
+    %w[Modules freetype jpeg png tiff].each do |feature|
+      assert_match feature, features
+    end
+  end
+end
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# homebrew-imagemagick
-A Homebrew tap for ImageMagick 6.7.8-9
+# A Homebrew tap for ImageMagick 6.7.8-9
+Based on the imagemagick@6 formula from this tap:  
+[homebrew-core](https://github.com/Homebrew/homebrew-core/blob/master/Formula/imagemagick@6.rb).
+
+<br/>Install imagemagick 6.7.8-9 from this tap by running:
+```bash
+brew install shlomiuziel/imagemagick/imagemagick@6.7.8-9
+```
+
+Once imagemagick was built successfully, you can link it as the default compare/identify system-wide command by invoking:
+
+```bash
+brew link --force imagemagick@6.7.8-9
+```
+
+#### (This step is required because it's a keg-only formula.)
